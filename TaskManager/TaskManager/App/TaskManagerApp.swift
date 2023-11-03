cb73502b869b80c860f95be6f2cae77d8d24a9ab
Dmitry Maslennikov <mrmda28@gmail.com>
2023-11-03 20:05:06 +0300

    Add a new project

---
+++ TaskManager/TaskManager/App/TaskManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagerApp.swift
+//  TaskManager
+//
+//  Created by Dmitry Maslennikov on 03.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
